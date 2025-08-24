810378683f536ee8b82aa95810f0e9784bcbd74c
Camusmarkdave <camusmarkdave@gmail.com>
2025-08-24 16:24:15 +0800

    updated: done!

--- index.js
+++ index.js
@@ -13,14 +13,38 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function (req, res) { 
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint... 
+// http://expressjs.com/en/starter/basic-routing.html
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+// Timestamp API
+app.get("/api/:date?", (req, res) => {
+  let dateString = req.params.date;
+  let date;
+
+  if (!dateString) {
+    date = new Date();
+  } else {
+    if (/^\d+$/.test(dateString)) {
+      date = new Date(parseInt(dateString));
+  } else {
+    date = new Date(dateString);
+  }
+}
+
+if (date.toString() === "Invalid Date") {
+  return res.json({ error: "Invalid Date"});
+}
+
+res.json ({
+  unix: date.getTime(),
+  utc: date.toUTCString()
+  });
 });
 
 // Listen on port set in environment variable or default to 3000
